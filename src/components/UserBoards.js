55e4ec53edb9112034c938408201ca117d425aec
Naftali Kulik <nkulik1994@gmail.com>
2022-07-05 15:19:58 -0400

    started filling out user boards component

--- src/components/UserBoards.js
+++ src/components/UserBoards.js
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Link } from 'react-router-dom'
+import { UserContext } from '../context/user';
 
 function UserBoards() {
+    const user = useContext(UserContext).user
+    if (!user) return <p className="login-msg">Please <Link to='/'>log in</Link> to view your boards</p>
+    
+    console.log(user)
+
+
     return <h1>My Boards</h1>
 }
 
