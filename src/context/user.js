3a84b6b2b7d13624262c92dca12821af1a722f91
Naftali Kulik <nkulik1994@gmail.com>
2022-07-05 10:08:30 -0400

    added context file for user

---
+++ src/context/user.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const UserContext = React.createContext();
+
+function UserProvider({ children }) {
+    const [user, setUser] = useState(null)
+    
+    function getAndSetUser(body, handleError) {
+        const config = {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(body)
+        }
+        fetch('http://localhost:4000/users', config)
+            .then(r => r.json())
+            .then(user => setUser(user))
+            .catch(handleError)
+    }
+    const currentUser = {
+        user,
+        setUser,
+        getAndSetUser
+    }
+
+    return <UserContext.Provider value={currentUser}>{children}</UserContext.Provider>;
+}
+
+export { UserContext, UserProvider };
