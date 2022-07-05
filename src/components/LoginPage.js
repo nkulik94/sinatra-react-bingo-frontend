e3a04498cd239a2d7889ecb857b0714975c457b6
Naftali Kulik <nkulik1994@gmail.com>
2022-07-05 11:41:38 -0400

    created component to handle both login forms

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,63 +1,14 @@
-import React, { useState, useContext } from 'react';
-import { UserContext } from '../context/user';
-import { useHistory } from 'react-router-dom';
-import { Form, Button } from 'react-bootstrap'
+import React, { useState } from "react";
+import CreateAccount from "./CreateAccount";
+import LoginForm from "./LoginForm";
 
 function LoginPage() {
-    const userObj = useContext(UserContext)
-    
-    
-    const [formData, setFormData] = useState({
-        username: '',
-        password: ''
-    })
-    const [displayError, setError] = useState(false)
-
-    function handleForm(e) {
-        setFormData({...formData, [e.target.name]: e.target.value})
-    }
-
-    function handleError() {
-        setError(true)
-        setTimeout(() => setError(false), 3000)
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault()
-        userObj.getAndSetUser(formData, handleError)
-        setFormData({
-            username: '',
-            password: ''
-        })
-    }
+    const [form, setForm] = useState('login')
 
     return (
-        <Form className='account-form' onSubmit={handleSubmit}>
-            <Form.Group className='mb-3'>
-                <Form.Label htmlFor='username'>Username</Form.Label>
-                <Form.Control 
-                    type="text"
-                    name="username"
-                    value={formData.username}
-                    placeholder='Enter Username'
-                    onChange={handleForm}
-                />
-            </Form.Group>
-            <Form.Group className='mb-3'>
-                <Form.Label htmlFor='password'>Password</Form.Label>
-                <Form.Control
-                    type='password'
-                    name="password"
-                    value={formData.password}
-                    placeholder='Enter Password'
-                    onChange={handleForm}
-                />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Log In
-            </Button>
-            {displayError ? <p>Sorry, the username or password do not match our records, please try again</p> : null}
-        </Form>
+        <div>
+            {form === 'login' ? <LoginForm /> : <CreateAccount />}
+        </div>
     )
 }
 
