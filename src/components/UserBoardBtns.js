500995cd16c31908acd78198dcd0946a99d7db26
Naftali Kulik <nkulik1994@gmail.com>
2022-07-06 16:27:31 -0400

    programmed play again button

--- src/components/UserBoardBtns.js
+++ src/components/UserBoardBtns.js
@@ -15,10 +15,34 @@ function UserBoardBtns({ board }) {
         history.push('/play')
     }
 
+    function handlePlayAgain() {
+        const userBoards = userObj.userBoards
+        const setUserBoards = userObj.setBoards
+        const userId = userObj.user.id
+        const config = {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({
+                player_id: userId,
+                board_id: board.board_id
+            })
+        }
+
+        fetch('http://localhost:4000/played-boards', config)
+            .then(r => r.json())
+            .then(newBoard => {
+                boardObj.setBoard(newBoard)
+                setUserBoards([...userBoards, newBoard])
+                history.push('/play')
+            })
+    }
+
     return (
         <ButtonGroup >
             <Button style={{fontSize: 'small'}} disabled={filledSpaces.length === 25} onClick={handleContinue} >Continue Playing</Button>
-            <Button style={{fontSize: 'small'}} >Play Again</Button>
+            <Button style={{fontSize: 'small'}} onClick={handlePlayAgain} >Play Again</Button>
             <Button style={{fontSize: 'small'}} variant="danger" >Reset</Button>
         </ButtonGroup>
     )
